574aeaa63fb834df153e9028fa79446286c2e432
picomax <picomax@gmail.com>
2019-11-20 13:19:02 -0800

    Change the filter for model for the site.

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,6 @@ from .models import Post
 
 def post_list(request):
 #	return render(request, 'blog/post_list.html', {})
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
